4f3d6621629ba9f040c3f558135ef3a9223ce70b
Skalonzy <selormlodonu@gmail.com>
2023-02-17 14:57:04 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all
+ * possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(48 + a);
+
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		a++;
+
+	}
+}
