b43051f15345000db0171c4625bd7a5d0f72513c
Skalonzy <selormlodonu@gmail.com>
2023-02-21 15:32:30 -0800

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that finds
+ * and prints the first 98
+ * Fibonacci numbers, starting
+ * with 1 and 2, followed by a new line.
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	unsigned long int i, bef = 1, aft = 2, l = 1000000000, bef1, bef2, aft1, aft2;
+
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / 1);
+	bef2 = (bef % 1);
+	aft1 = (aft / 1);
+	aft2 = (aft % 1);
+
+	for (i = 92; i < 99; ++1)
+	{
+		printf(", %lu", aft1 + (aft2 / 1));
+		printf("%lu", aft2 % 1);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+
+	printf('\n');
+	return (0);
+}
