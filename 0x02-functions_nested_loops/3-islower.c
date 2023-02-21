dd02ab46bc73dab04e117e43f0be868791ce1b59
Skalonzy <selormlodonu@gmail.com>
2023-02-21 11:40:41 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 
 /**
  * _islower - Write a function that checks for lowercase character.
- * @c - character to check the case
+ * @c : character to check the case
  *
- * Return: 0
+ * Return: 0 or 1
  */
 
 int _islower(int c)
