24c9b83f4b565e984273e6c720cf7b787d8441b2
Skalonzy <selormlodonu@gmail.com>
2023-05-04 16:05:29 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,17 +13,13 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	unsigned int decimal_value = 0;
-	int power_of_two = 1;
 
-	for (; *b; b++)
+	for (int i = 0; b[i]; i++)
 	{
-		int digit = *b - '0';
-
-		if (digit < 0 || digit > 1)
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 
-		decimal_value += digit * power_of_two;
-		power_of_two *= 2;
+		decimal_value = 2 * decimal_value + (b[i] - '0');
 	}
 
 	return (decimal_value);
