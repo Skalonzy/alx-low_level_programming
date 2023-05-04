fb4aab4f2c885c96cbef28921612e432e1df4fd7
Skalonzy <selormlodonu@gmail.com>
2023-05-04 16:59:48 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,26 +7,8 @@ include "main.h"
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
 	int i = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while ((n >> (i + 1)) > 0)
-		i++;
-
-	while (i >= 0)
-	{
-		if ((n >> i) & mask)
-			_putchar('1');
-
-		else
-			_putchar('0');
-
-		i--;
-	}
+	for (i = 63; i >= 0; i--)
+		_putchar(((n >> i) & 1) + '0');
 }
