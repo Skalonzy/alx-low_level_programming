2adb28b1576463e406e28fb67cf02993f535bd96
Skalonzy <selormlodonu@gmail.com>
2023-05-04 15:54:22 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * b_to_u - print binary to unit
- * @binary_number: 1st parameter
+ * binary_to_uint - print binary to unit
+ * @b: 1st parameter
  *
  * Return: decimal value
  */
 
-unsigned int bnary_to_uint(const char *binary_number)
+unsigned int binary_to_uint(const char *b)
 {
-	if (!binary_number)
+	if (!b)
 		return (0);
 
 	unsigned int decimal_value = 0;
 	int power_of_two = 1;
 
-	for (; *binary_number; binary_number++)
+	for (; *b; b++)
 	{
-		int digit = *binary_number - '0';
+		int digit = *b - '0';
 
 		if (digit < 0 || digit > 1)
 			return (0);
