e2d4d573e45f6dcc3e4d06047404c18b795bc261
Skalonzy <selormlodonu@gmail.com>
2023-05-04 16:19:52 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,4 +9,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	if (!b)
+		return (0);
+
+	unsigned int dec_val = 0;
+
+	int i = 0;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0' || b[i] == '1')
+			dec_val = (dec_val << 1) | (b[i] - '0');
+
+		else
+			return (0);
+
+		i++;
+	}
+
+	return (dec_val);
 }
