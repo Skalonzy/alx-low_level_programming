f3d51c345e37d7244fc1787a02e2022522d85dba
Skalonzy <selormlodonu@gmail.com>
2023-05-08 15:50:10 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,60 +8,30 @@
  * Return: 0 when function fails or filename is NULL.
  */
 
-/**
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytes_read = 0;
+	ssize_t bytes_written = 0;
+
 	int fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	char *buf = malloc(letters + 1);
+	char ch = '\0';
+	size_t i = 0;
 
-	if (!buf)
+	while (i < letters && (bytes_read = read(fd, &ch, 1)) > 0)
 	{
-		close(fd);
-		return (0);
+		bytes_written = write(STDOUT_FILENO, &ch, 1);
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (0);
+		}
+		i++;
 	}
-
-
-	for (size_t i = 0; i < letters + 1; i++)
-		buf[i] = '\0';
-
-	bytes_read = read(fd, buf, letters);
-
-	if (bytes_read == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-
-	free(buf);
 	close(fd);
-
 	return (bytes_written);
-}
-*/
-
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	char *buf;
-	ssize_t fd, w, t;
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
-	close(fd);
-	return (w);
 }
