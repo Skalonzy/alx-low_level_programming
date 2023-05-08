de24fc4624512b6f00b09c7fe399b46f0d6dec98
Skalonzy <selormlodonu@gmail.com>
2023-05-08 15:31:40 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,6 +8,7 @@
  * Return: 0 when function fails or filename is NULL.
  */
 
+/**
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytes_read = 0;
@@ -44,4 +45,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	return (bytes_written);
 }
+*/
 
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, w, t;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
