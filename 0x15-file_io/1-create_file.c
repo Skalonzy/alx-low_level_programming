2339ff529b586823471ffac346393e2788d388e1
Skalonzy <selormlodonu@gmail.com>
2023-05-08 17:08:22 -0700

    T

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,28 +5,29 @@
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
- * Return: If the function fails - -1. Otherwise - 1.
+ * Return: - 1 on fail.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int a, b, c = 0;
-
-	if (text_content != NULL)
-	{
-		for (c = 0; text_content[c];)
-			c++;
-	}
+	int aa, bb, cc = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	a = opem(filename, O_CREAT | O_RDWR | 0_TRUNC, 0600);
-	b = write(a, text_content, c);
+	if (text_content != NULL)
+	{
+		for (cc = 0; text_content[cc];)
+			c++;
+	}
 
-	if (a == -1 || b == -1)
+	aa = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bb = write(aa, text_content, cc);
+
+	if (aa == -1 || bb == -1)
 		return (-1);
 
-	close(a);
+	close(aa);
+
 	return (1);
 }
