6c80f7812ea7d13f23c9ad3bdb4494785cc6153f
Skalonzy <selormlodonu@gmail.com>
2023-05-08 17:21:35 -0700

    tt

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (cc = 0; text_content[cc];)
-			c++;
+			cc++;
 	}
 
 	aa = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
